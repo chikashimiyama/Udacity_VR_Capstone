b4d0b8ead667d502d7734fe27db64e43d5e85c9a
Chikashi Miyama <chikashimiyama@gmail.com>
2018-12-08 21:27:21 +0100

    add raycast

--- project/Capstone/Assets/Scripts/Controller/PointerBehaviour.cs
+++ project/Capstone/Assets/Scripts/Controller/PointerBehaviour.cs
@@ -4,14 +4,23 @@ namespace DomainF
 {
     public class PointerBehaviour : MonoBehaviour
     {
-        
-        public void OnTransformChanged()
-        {
-            
-        }
-        
-        
+        private LineRenderer laserPointer_;
 
+        private void Start()
+        {
+            laserPointer_ = GetComponentInChildren<LineRenderer>();
+        }
+
+        private void Update()
+        {
+            laserPointer_.SetPosition(0, transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 50))
+            {
+                Debug.Log("hit");
+                laserPointer_.SetPosition(1, hit.point);
+            }
+        }
     }
 
 
