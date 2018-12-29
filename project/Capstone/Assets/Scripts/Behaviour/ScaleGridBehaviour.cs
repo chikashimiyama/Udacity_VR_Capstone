667981ac3eadff1235729e3e071f377034953672
Chikashi Miyama <chikashimiyama@gmail.com>
2018-12-29 12:00:36 +0900

    add scale grid prefab

---
+++ project/Capstone/Assets/Scripts/Behaviour/ScaleGridBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class ScaleGridBehaviour : MonoBehaviour {
+
+	[SerializeField] private GameObject circlePrefab;
+	private const int NumPitches = 24;
+	private const float Step = 180f / NumPitches;
+	private const float MaxDist = 35f;
+	private const float VStep = MaxDist * 2 / NumPitches;
+
+	private void Start ()
+	{
+		var scl = Step;
+		var yPos = -MaxDist + VStep;
+		for (var i = 1; i < NumPitches; i++)
+		{
+			var circle = Instantiate(circlePrefab);
+			circle.transform.parent = gameObject.transform;
+
+			var scale = (float)Math.Sin(Mathf.Deg2Rad *scl ) * MaxDist;
+			circle.transform.localScale = new Vector3(scale, 1f, scale);
+			circle.transform.position = new Vector3(0, yPos, 0);
+			scl += Step;
+			yPos += VStep;
+		}
+	}
+	
+}
