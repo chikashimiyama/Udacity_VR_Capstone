c3f4ad3ccfd154d72942333ca863895bcc958aff
Chikashi Miyama <chikashimiyama@gmail.com>
2019-01-06 12:38:02 +0100

    add new prefabs

---
+++ project/Capstone/Assets/Scripts/Behaviour/EquatorBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace DomainF
+{
+    public  interface IColor
+    {
+        Color color { set; }
+    }
+    
+    public class EquatorBehaviour : MonoBehaviour, IShowable, IColor
+    {
+        [SerializeField] private GameObject equator;
+
+        private void Start()
+        {
+        }
+        
+        
+        public bool Visible
+        {
+            set { equator.SetActive(value); }
+        }
+
+        public Color color
+        {
+            set
+            {
+                
+            }
+        }
+    }
+}
---
+++ project/Capstone/Assets/Scripts/Behaviour/WaveformInterpolationBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DomainFo
+{
+	public interface IWaveformInterpolationBehaviour
+	{
+		float Angle { set; }
+	}
+
+	public class WaveformInterpolationBehaviour : MonoBehaviour, IWaveformInterpolationBehaviour
+	{
+		
+		public float Angle
+		{
+			set
+			{
+				var euler = transform.eulerAngles;
+				euler.z = value;
+				transform.eulerAngles = euler;
+			}
+		}
+	}
+
+
+}
